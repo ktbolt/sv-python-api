62ef17b358e4c44b4b12fba6e42b0cf3b3938811
Dave Parker <davep@stanford.edu>
2019-12-02 16:30:23 -0800

    Add contour kernel.

--- tests/contour/contour.py
+++ tests/contour/contour.py
@@ -1,7 +1,12 @@
 import sv
 
-print(dir(sv))
+print(dir(sv.contour))
+print("Contour kernel names: {0:s}".format(str(sv.contour.kernel.names)))
 
-cont = sv.contour.Contour()
-
+## Create CircleContour using factory.
+#
+#center = [0.0, 0.0, 0.0]
+cont = sv.contour.create(sv.contour.kernel.CIRCLE)
+#cont.set_center(center)
+#cont.set_radius(1.0)
 
