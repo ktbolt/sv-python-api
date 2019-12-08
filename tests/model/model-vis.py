0420b518316eba9289488b0f1342d1a234f8fddd
Dave Parker <davep@stanford.edu>
2019-12-07 20:41:36 -0800

    Add modeling tests.

---
+++ tests/model/model-vis.py
@@ -0,0 +1,45 @@
+import sv
+import vtk
+
+print("Solid modeling kernel names: {0:s}".format(str(sv.solid.Kernel.names)))
+
+## Create a cylinder.
+#
+print("Create a cylinder.") 
+center = [0.0, 0.0, 0.0]
+axis = [0.0, 0.0, 1.0]
+radius = 1.5
+length = 10.0
+model = sv.solid.Solid(sv.solid.Kernel.POLYDATA)
+model.cylinder(radius, length, center, axis)
+polydata = model.get_polydata() 
+
+## Create renderer and graphics window.
+#
+renderer = vtk.vtkRenderer()
+renderer_win = vtk.vtkRenderWindow()
+renderer_win.AddRenderer(renderer)
+renderer.SetBackground(0.8, 0.8, 0.8)
+renderer_win.SetSize(500, 500)
+renderer_win.Render()
+renderer_win.SetWindowName("Vis SV Solid Model")
+
+# Add cylinder polydata.
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputData(polydata)
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetColor(1.0, 0.0, 0.0)
+actor.GetProperty().SetPointSize(5)
+renderer.AddActor(actor)
+
+## Create a trackball interacter to transoform the geometry using the mouse.
+#
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+interactor.SetRenderWindow(renderer_win)
+interactor.Start()
+
+
+
+
--- tests/model/model.py
+++ tests/model/model.py
@@ -1,9 +1,32 @@
 import sv
+import vtk
 
-print(dir(sv.contour))
-#print("Contour kernel names: {0:s}".format(str(sv.contour.Kernel.names)))
+#print(dir(sv))
+print(dir(sv.solid))
+print("Solid modeling kernel names: {0:s}".format(str(sv.solid.Kernel.names)))
 
 ## Create model. 
 #
-model = sv.solid.SolidModel()
+#model = sv.solid.SolidModel()
+#model = sv.solid.SolidModel("bob")
+#model = sv.solid.SolidModel(sv.solid.Kernel.PARASOLID)
+
+#model = sv.solid.PolyData()
+
+#model = sv.solid.Solid(sv.solid.Kernel.POLYDATA)
+
+#print("Model type: {0:s}".format(str(type(model))))
+
+## Create a cylinder.
+#
+print("Create a cylinder.") 
+center = [0.0, 0.0, 0.0]
+axis = [0.0, 0.0, 1.0]
+radius = 1.5
+length = 10.0
+model = sv.solid.Solid(sv.solid.Kernel.POLYDATA)
+model.cylinder(radius, length, center, axis)
+cyl_polydata = model.get_polydata() 
+print("polydata: " + str(type(cyl_polydata)))
+print("Cylinder: num nodes: {0:d}".format(cyl_polydata.GetNumberOfPoints()))
 
