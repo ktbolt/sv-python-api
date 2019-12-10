3d1e6db87881be62cf6cb1c6d49e9f55b2a04d4f
Dave Parker <davep@stanford.edu>
2019-12-09 16:28:36 -0800

    Add 'modeler' class.

--- tests/model/model.py
+++ tests/model/model.py
@@ -2,20 +2,12 @@ import sv
 import vtk
 
 #print(dir(sv))
-print(dir(sv.solid))
+#print(dir(sv.solid))
 print("Solid modeling kernel names: {0:s}".format(str(sv.solid.Kernel.names)))
 
-## Create model. 
+## Create a polydata modeler
 #
-#model = sv.solid.SolidModel()
-#model = sv.solid.SolidModel("bob")
-#model = sv.solid.SolidModel(sv.solid.Kernel.PARASOLID)
-
-#model = sv.solid.PolyData()
-
-#model = sv.solid.Solid(sv.solid.Kernel.POLYDATA)
-
-#print("Model type: {0:s}".format(str(type(model))))
+modeler = sv.solid.Modeler(sv.solid.Kernel.POLYDATA)
 
 ## Create a cylinder.
 #
@@ -24,9 +16,24 @@ center = [0.0, 0.0, 0.0]
 axis = [0.0, 0.0, 1.0]
 radius = 1.5
 length = 10.0
-model = sv.solid.Solid(sv.solid.Kernel.POLYDATA)
-model.cylinder(radius, length, center, axis)
-cyl_polydata = model.get_polydata() 
-print("polydata: " + str(type(cyl_polydata)))
-print("Cylinder: num nodes: {0:d}".format(cyl_polydata.GetNumberOfPoints()))
+cyl = modeler.cylinder(radius, length, center, axis)
+#cyl_polydata = model.get_polydata() 
+#print("polydata: " + str(type(cyl_polydata)))
+#print("Cylinder: num nodes: {0:d}".format(cyl_polydata.GetNumberOfPoints()))
+
+'''
+
+## Modeler class creates objects and performs operations 
+#  on them, returns SolidModel object.
+#
+modeler = sv.solid.Modeler(sv.solid.Kernel.POLYDATA)
+
+cyl_polydata = cyl.get_polydata() 
+
+box = modeler.box([h,w,d], center)
+
+box_minus_cyl = modeler.subtract(box, cyl)
+
+
+'''
 
