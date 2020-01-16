f531d8055a83145081878b39a3dd200a0bb35622
Dave Parker <davep@stanford.edu>
2020-01-15 21:43:08 -0800

    Add meshing test.

--- tests/mesh/tetgen-mesh.py
+++ tests/mesh/tetgen-mesh.py
@@ -21,12 +21,15 @@ print(mesher.available())
 ## Set meshing options.
 #
 print("Set meshing options ... ")
-options = mesher.create_options()
+options = mesher.create_options(global_edge_size=0.1, surface_mesh_flag=1, volume_mesh_flag=1, mesh_wall_first=1)
 #options.global_edge_size = "a"
+#options.global_edge_size = 0.5
+options.epsilon = 0.5
+
 print("options.global_edge_size: " + str(options.global_edge_size))
 
-options_values = options.get_values() 
-print("options values: " + str(options_values))
+print("options values: ")
+[ print("  {0:s}:{1:s}".format(key,str(value))) for (key, value) in sorted(options.get_values().items()) ]
 
 #help(options)
 mesher.set_options(options)
