1189a14aed3a4424bd5270af87685e15bfddca1d
Dave Parker <davep@stanford.edu>
2020-02-20 17:23:58 -0800

    Add get mesh, logging.

--- tests/mesh/tetgen-adapt.py
+++ tests/mesh/tetgen-adapt.py
@@ -2,6 +2,7 @@
 Test TetGen adaptive meshing interface.
 '''
 import sv
+import sys
 import vtk
 import graphics as gr
 
@@ -33,42 +34,11 @@ print("options values: ")
 #
 results_file_name = 'all_results.vtu'
 model_file_name = 'cylinder.vtp'
-adaptive_mesher.generate_mesh(results_file=results_file_name, model_file=model_file_name, options=options)
+adaptive_mesher.generate_mesh(results_file=results_file_name, model_file=model_file_name, options=options, log_file='mesher.log')
 
-#adaptive_mesher.load_model(file_name)
+## Get the mesh.
+#
+mesh = adaptive_mesher.get_mesh()
+print("Number of adaptive mesh nodes: {0:d}".format(mesh.GetNumberOfPoints()))
+print("Number of adaptive mesh elements: {0:d}".format(mesh.GetNumberOfCells()))
 
-'''
-
-## Set mesher options.
-mesher.set_options(options)
-
-## Set the face IDs for model walls.
-face_ids = ['a']
-face_ids = 1
-face_ids = []
-face_ids = [1]
-mesher.set_walls(face_ids)
-
-
-## Compute model boundary faces.
-mesher.compute_model_boundary_faces(angle=60.0)
-face_info = mesher.get_model_face_info()
-print("Mesh face info: " + face_info);
-
-## Generate the mesh. 
-mesher.generate_mesh()
-
-## Get the mesh as a vtkUnstructuredGrid. 
-mesh = mesher.get_mesh()
-print("Mesh:");
-print("  Number of nodes: {0:d}".format(mesh.GetNumberOfPoints()))
-print("  Number of elements: {0:d}".format(mesh.GetNumberOfCells()))
-
-## Mesh adapt? does not work.
-#mesher.adapt()
-#mesh = mesher.get_mesh()
-#print("Mesh:");
-#print("  Number of nodes: {0:d}".format(mesh.GetNumberOfPoints()))
-#print("  Number of elements: {0:d}".format(mesh.GetNumberOfCells()))
-
-'''
