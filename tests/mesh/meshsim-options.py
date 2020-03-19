050c1e428c4a17ef94e858154ff3308a8962e7fe
Dave Parker <davep@stanford.edu>
2020-03-18 19:49:01 -0700

    Add more testing and options processing in c++.

--- tests/mesh/meshsim-options.py
+++ tests/mesh/meshsim-options.py
@@ -17,6 +17,15 @@ options = sv.meshing.pMeshSimOptions(global_edge_size=global_edge_size, surface_
 #print(dir(options.local_edge_size))
 
 #------------------
+# global_curvature 
+#------------------
+if False:
+    print(" ")
+    print("Set global curvature ... ")
+    options.global_curvature = {'curvature':4.0, 'absolute':True }
+    print("Global curvature: " + str(options.global_curvature))
+
+#------------------
 # global_edge_size 
 #------------------
 if False:
@@ -37,20 +46,70 @@ if False:
     options.local_edge_size =  [ {'face_id':1, 'edge_size':1.0, 'absolute':True } ]
     options.local_edge_size.append( {'face_id':2, 'edge_size':2.0, 'absolute':True } ) 
     # Test error conditions.
+    options.local_edge_size =  {'face_id':1, 'edge_size':1.0, 'absolute':True }
     options.local_edge_size.append( {'face_id':2, 'edge_siz':2.0, 'absolute':True } ) 
+
+#-----------------
+# local_curvature 
+#-----------------
+if False:
+    print("Set local curvature ... ")
+    options.local_curvature =  [ {'face_id':1, 'curvature':1.0, 'absolute':True } ]
+    options.local_curvature.append( {'face_id':2, 'curvature':2.0, 'absolute':True } )
+    # Test error conditions.
+    options.local_curvature.append( {'id':2, 'curvature':2.0, 'absolute':True } )
+
+#--------------------
+# local_min_curvature 
+#--------------------
+if True:
+    print("Set local min curvature ... ")
+    options.local_min_curvature =  [ {'face_id':1, 'min_curvature':1.0, 'absolute':True } ]
+    options.local_min_curvature.append( {'face_id':2, 'min_curvature':2.0, 'absolute':True } )
 
 #-------------------
 # surface_mesh_flag
 #-------------------
 if False:
+    print("Set surface mesh flag ... ")
     options.surface_mesh_flag = True 
     # Test error conditions.
     #options.surface_mesh_flag = 1 
 
+#----------------------
+# surface_optimization 
+#----------------------
+if True:
+    print("Set surface optimization ... ")
+    options.surface_optimization = 2
+    # Test error conditions.
+    options.surface_optimization = -1
+
+#------------------
+# volume_mesh_flag
+#------------------
+if False:
+    print("Set volume mesh flag ... ")
+    options.volume_mesh_flag = True 
+    # Test error conditions.
+
+#----------------------
+# volume_optimization 
+#----------------------
+if False:
+    print("Set volume optimization ... ")
+    options.volume_optimization = 4
+    # Test error conditions.
+
 #---------------
 # print options 
 #---------------
-print("----------")
 print("Options ... ")
-options.print()
+values = options.get_values()
+for k,v in values.items():
+    print("{0:s}: {1:s}".format(k,str(v)))
+
+c_options = sv.meshing.MeshSimOptions(global_edge_size=global_edge_size, surface_mesh_flag=True, volume_mesh_flag=True)
+#c_options.parse_options(options)
+
 
