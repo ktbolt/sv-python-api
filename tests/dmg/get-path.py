70d607797aee3e4bd4cf266eed07efd2692df0dc
Dave Parker <davep@stanford.edu>
2020-04-28 21:05:25 -0700

    Add 'add_geometry' method test.

--- tests/dmg/get-path.py
+++ tests/dmg/get-path.py
@@ -3,7 +3,6 @@ Test getting a path from the SV Data Manager.
 
 This is tested using the Demo Project.
 '''
-
 import sv
   
 ## Create a Python path object from the SV Data Manager 'Paths/aorta' node. 
@@ -11,6 +10,7 @@ import sv
 path_name = "aorta"
 path = sv.dmg.get_path(path_name)
 control_points = path.get_control_points()
+print("Get '{0:s}' path.".format(path_name))
 print("Number of control points: {0:d}".format(len(control_points)))
 print("Control points: ")
 for i,pt in enumerate(control_points):
